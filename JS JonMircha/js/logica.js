c59882cd139930fed862e3a6f2b16eddfbcd3ebf
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2023-11-29 17:52:20 -0400

    minor change

--- JS JonMircha/js/logica.js
+++ JS JonMircha/js/logica.js
@@ -132,6 +132,18 @@ const eleminarCaracter = (texto = "", patron = "") =>
 
 const aleatorio = () => console.info(Math.round(Math.random() * 100) + 500)
 
+<<<<<<< HEAD
+aleatorio()
+
+
+/*
+10)
+Programa una función que reciba un número y evalúe si es capicúa o no (que se
+lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true. 11)
+Programa una función que calcule el factorial de un número (El factorial de
+un entero positivo n, se define como el producto de todos los números enteros
+positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
+=======
 //aleatorio()
 
 //Ejercicio 10
@@ -217,4 +229,5 @@ numeroPrimo(5)
 Programa una función que determine si un número es par o impar, pe. miFuncion
 (29) devolverá Impar. 14) Programa una función para convertir grados Celsius
 a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.
+>>>>>>> jonMicha
 */
