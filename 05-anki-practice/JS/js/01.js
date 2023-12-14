c46517cac9d51f7c8836de6fead2875cbbe52805
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2023-12-14 11:34:11 -0400

    anki practice - generator (function*)

--- 05-anki-practice/JS/js/01.js
+++ 05-anki-practice/JS/js/01.js
@@ -7,3 +7,21 @@ const wives = {};
 esposas.forEach((esposa,i) => wives[`id_${i}`] = esposa);
 
 console.log(wives.id_0);
+
+
+//funciones generadoras
+
+function* generador(){
+  yield "primer yield";
+  console.log("seguimos");
+  yield "el segundo yield";
+  console.log("ultima tarea");  
+}
+
+let yields = generador();
+
+for (const iterator of yields) {
+  console.log(iterator);
+}
+
+const arr = [...generador()]
