474ac6c70514e903d648a8e923192d902d08384e
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2023-12-15 07:31:05 -0400

    anki practice - random practice

--- 05-anki-practice/anki.js
+++ 05-anki-practice/anki.js
@@ -1,136 +1,61 @@
-//propiedades dinamicas
-const usuarios = ["carlos","jesus","julio"]
+//Funcion constructora
 
-const objUsuarios = {}
-
-usuarios.forEach((usuario,i) => objUsuarios[`id_${i}`] = usuario)
-
-console.log(objUsuarios.id_2)
-
-//funciones generadoras
-
-function* iterable(){
-  yield "soy el primer yield"
-  console.log("sigo con una tarea")
-  yield "segundo yield, aqui para el conteo"
-  console.log("esto esta fuera del conteo")
-  yield "tecer yield"
+/*function Perro(n,r,t){
+  this.super = Animal
+  this.super(n,r)
+  this.tamanio = t
 }
 
-const arregloGenerador = [...iterable()];
+Perro.prototype = new Animal
+Perro.prototype.constructor = Perro
+*/
 
-console.log(arregloGenerador);
+//cortocircuito and
+console.log(false && "derecha")
 
-// funcion AE jquery
+//variables. 
 
-(function (d, w, c) {
-  console.log("Mi segunda IIFE");
-  console.log(d);
-  console.log(w);
-  c.log("Este es un console.log")
-})(document, window, console);
+const $bgColor = window.getComputedStyle(document.documentElement).getPropertyValue("--bg-color")
+const $fontSize = window.getComputedStyle(document.documentElement).getPropertyValue("--font-size")
 
+console.log($bgColor)
+console.log($fontSize)
 
-//iteradores
+//setear valores 
+document.body.style.backgroundColor = $bgColor
 
-const iterador = usuarios[Symbol.iterator]()
+//accder a una clase css
 
-console.log(iterador.next())
-
-//set 
-
-let set = new Set();
-
-set.add('Jonathan');
-set.add('Irma');
-set.add('Irma');
-//Imprime ["Jonathan", "Jonathan"]
-//Imprime ["Irma", "Irma"]
-
-for ([i,u] of set.entries()) {
-  console.log(i,u)
-}
-
-for (item of set.entries()){
-  console.log(item)
-}
-
-//comodines console
-
-let string = "STRINGOTE"
-console.log("hola este es un string %s: wow que string", string)
-//=> hola este es un string STRINGOTE: wow que string
-
-//sets
-
-let seteo = new Set(["hola",22,"casa"])
-
-console.log(seteo)
+let $primerParrafo = document.getElementById("primerParrafo")
 
 
-//push y unshif
-
-const hermanas = []
-
-hermanas.push("Pelua") // segunda
-hermanas.push("Maye") //tercera
-hermanas.unshift("Mariyen") //primera
-
-console.log(hermanas)
-
-//agrego elemento hijo al final
-
-let elementoHijo = document.createElement("p")
-let div = document.getElementById("div")
-elementoHijo.textContent = "Soy el texto del ultimo hijo del div"
+console.log($primerParrafo.classList)
+console.log($primerParrafo.classList.contains("miClase") + " si tiene la clase")
+console.log($primerParrafo.getAttribute("class"))
+console.log($primerParrafo.className) // al parecer estas dos ultimas hacen lo mismo con las clases
 
 
-div.appendChild(elementoHijo)
+$primerParrafo.classList.replace("miClase","claseReemplazada")
 
-// seguimos con interadores 
-
-const iterables = [1,2,3,4,5,6,7,8,9];
-const iterator = iterables[Symbol.iterator]()
-
-//Recorrer con un siclo while el arreglo con el iterador
-
-let next = iterator.next()
-
-while(!next.done){
-  console.log(next.value)
-  next = iterator.next()
-}
-
-//mapas
+console.log($primerParrafo.className) 
 
 
-const hermanasMap = new Map([["mayor","Mariyen"],["mediana","Pelua"],["Pequeña","Maye"]])
+//textos
 
-const nombresHer = [...hermanasMap.values()]
+let text = `
+    <p>
+    El Modelo de Objetos del Documento (<b><i>DOM - Document Object Model </i></b>) es un                    
+API para documentos HTML y XML.
+    </p>
+    <p>
+    Éste provée una representación estructural del documento, permitiendo modificar su contenido y presentación visual mediante código JS.
+    </p>
+    <p>
+        <mark> El DOM no es parte de la especificación de JavaScript, es una API para los navegadores.</mark>
+    </p>
+`
 
-console.log(nombresHer)
-
-
-// los proxys 
-
-const obj = {
-  "nombre": "",
-  "edad": ""
-}
-
-
-const manejador = {
-  set(obj,prop,value){
-    if(Object.keys(obj).indexOf(prop) === -1){
-      return console.error("no exite esa propiedad")
-    }
-    obj[prop] = value
-  }
-}
-
-const objProxy = new Proxy(obj,manejador)
-
-objProxy.edad = 31
-objProxy.casado = true
-
-console.log(objProxy.edad)
+$primerParrafo.innerHTML = text
+$primerParrafo.outerHTML = text // para este caso es mejor
+//$primerParrafo.innerText = text
+//$primerParrafo.textContent = text
