8b8b8dee622468c4eeed6fda94c91a6db9896897
CarlosJavierOrozco <carlosjorozco.jw@gmail.com>
2024-01-12 12:11:38 -0400

    variables in java

--- 06-pildoras-informaticas/00-java/FirstSteps/src/FirstSteps.java
+++ 06-pildoras-informaticas/00-java/FirstSteps/src/FirstSteps.java
@@ -3,7 +3,7 @@ public class FirstSteps {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hola Mundo!");
+		System.out.println("Hola Mundo!");
 	}
 
 }
---
+++ 06-pildoras-informaticas/00-java/FirstSteps/src/Variables.java
@@ -0,0 +1,12 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		//iniciar variables
+		byte edad;
+		edad = 32;
+		
+		System.out.print(edad);
+	}
+
+}
