b841ad162134e84e5ca726a2244e1dd5840e8336
CarlosJavierOrozco <carlosjorozco.jw@gmail.com>
2024-01-15 13:01:03 -0400

    Create Entrada_Formato.java

---
+++ 06-pildoras-informaticas/00-java/FirstSteps/src/Entrada_Formato.java
@@ -0,0 +1,12 @@
+
+public class Entrada_Formato {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double x = 100000.0;
+		
+		System.out.printf("%1.3f", x/3);
+	}
+
+}
