bfef520c448c59d9c0099c9fe9560cb35f54d80e
CarlosJavierOrozco <carlosjorozco.jw@gmail.com>
2024-01-25 12:41:25 -0400

    second java class in edTeam
    conditionals, numbers, logical operators

---
+++ 02-edteam/10-curso-java-2016/holamundo/Numeros.java
@@ -0,0 +1,21 @@
+public class Numeros {
+    public static void main(String[] args) {
+        int num = 0x212;
+        int bin = 0b11111111;
+        System.out.println(num);
+        System.out.println(bin);
+
+        float a = 5, b = 2;
+        
+        float sum = a / b;
+        float sum2 = 5f / 2f;
+        
+        System.out.println(sum);
+        System.out.println(sum2);
+
+
+        //todos lo flotantes java los trata como double
+
+
+    }
+}
